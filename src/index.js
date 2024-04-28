aed7f16a9c790eedd9c11b05f2d85edaa22cc0e4
yuav8520 <yuav8520.gmail.com>
2024-04-28 18:29:13 +0300

    need to go

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("hellodasds d");
+
+const body=document.querySelector('body');
+const content = document.createElement('div');
+const header=document.createElement('header');
+const nav=document.createElement('nav');
+const h2=document.createElement('h2');
+const img = document.createElement('img');
+const div=document.createElement('div');
+
+img.src = '../src/images/pizza.jpeg';
+content.id='content';
+h2.textContent="Tzuber's pizza";
+div.textContent="this is the best pizza there is come join us in ...";
+
+header.append(nav);
+content.append(header);
+content.append(h2);
+content.append(img);
+content.append(div);
+
+body.append(content);
+
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean:true,
 
+
   },
   plugins: [
     new HtmlWebpackPlugin({
