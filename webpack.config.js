fff0a1ee9a72c93b33b3877a99a97d624c3ca83e
yuav8520 <yuav8520.gmail.com>
2024-04-28 18:06:26 +0300

    adding css to js

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { watch } = require('fs');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean:true,
+
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template: 'src/index.html', // Path to your HTML template
+        template: 'src/index.html',
+        filename: 'main.html',// Path to your HTML template
     }),],
 };
